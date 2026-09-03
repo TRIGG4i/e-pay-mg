d0c7ae945911fec64c865ed99119a9ffd6cdfcad
TRIGG4i <tl.rakotondravony@gmail.com>
2026-09-03 15:14:47 +0300

    Load interface theme controls

--- assets/app.js
+++ assets/app.js
@@ -19,7 +19,7 @@
   loadScript("assets/app-core.js")
     .then(() => loadScript("assets/internal-fee-patch.js"))
     .then(() => loadScript("assets/customer-quote-patch.js"))
-    .then(() => loadScript("assets/export-image-patch.js"))
+    .then(() => loadScript("assets/export-image-patch.js"))\n    .then(() => loadScript("assets/theme-patch.js"))
     .then(() => loadScript("assets/motion-polish.js"))
     .catch((error) => console.error("E-Pay MG :", error));
 })();
